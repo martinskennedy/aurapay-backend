78f92012e195c35d64bbad4d53684c4ec522733f
Kennedy Martins <kemartins@gmail.com>
2026-03-02 11:35:10 -0300

    feat: implement TransactionService with unit of work for transfers

---
+++ src/AuraPay.Application/Services/TransactionService.cs
@@ -0,0 +1,56 @@
+﻿using AuraPay.Application.DTOs;
+using AuraPay.Application.Interfaces;
+using AuraPay.Domain.Entities;
+using AuraPay.Domain.Enums;
+using AuraPay.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraPay.Application.Services
+{
+    internal class TransactionService : ITransactionService
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly ITransactionRepository _transactionRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TransactionService(IAccountRepository accountRepository, ITransactionRepository transactionRepository, IUnitOfWork unitOfWork)
+        {
+            _accountRepository = accountRepository;
+            _transactionRepository = transactionRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> TransferAsync(Guid originUserId, TransferRequestDto request)
+        {
+            // 1. Buscar conta de origem pelo UserId (quem está logado)
+            var originAccount = await _accountRepository.GetByUserIdAsync(originUserId);
+            if (originAccount == null) throw new Exception("Conta de origem não encontrada.");
+
+            // 2. Buscar conta de destino pelo Id informado
+            var destinationAccount = await _accountRepository.GetByIdAsync(request.DestinationAccountId);
+            if (destinationAccount == null) throw new Exception("Conta de destino não encontrada.");
+
+            // 3. Executar a lógica de negócio (Regras de Domínio)
+            // O domínio valida se há saldo suficiente
+            originAccount.Withdraw(request.Amount);
+            destinationAccount.Deposit(request.Amount);
+
+            // 4. Criar os registros de extrato (Auditoria)
+            var debitNote = new Transaction(originAccount.Id, request.Amount, TransactionType.TransferOut);
+            var creditNote = new Transaction(destinationAccount.Id, request.Amount, TransactionType.TransferIn);
+
+            await _transactionRepository.AddAsync(debitNote);
+            await _transactionRepository.AddAsync(creditNote);
+
+            // 5. Persistir as mudanças no banco de forma ATÔMICA
+            // O CommitAsync salva as duas contas e as duas transações ou NADA caso ocorra algum erro.
+            var result = await _unitOfWork.CommitAsync();
+
+            return result > 0;
+        }
+    }
+}
