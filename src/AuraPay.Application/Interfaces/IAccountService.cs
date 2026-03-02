78e4bcc885671e5b6d6bdcff4fa6d9ea7843e3c8
Kennedy Martins <kemartins@gmail.com>
2026-03-02 11:13:11 -0300

    feat: add Application DTOs and service interfaces

---
+++ src/AuraPay.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using AuraPay.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraPay.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<AccountDto> CreateAccountAsync(Guid userId);
+        Task<AccountDto?> GetBalanceAsync(Guid userId);
+    }
+}
