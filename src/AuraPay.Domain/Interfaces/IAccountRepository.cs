5f7435d690d95fcf177f5355e5c6d55c86d0a09b
Kennedy Martins <kemartins@gmail.com>
2026-03-02 09:55:42 -0300

    feat: add repository interfaces and Unit of Work contract

---
+++ src/AuraPay.Domain/Interfaces/IAccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using AuraPay.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraPay.Domain.Interfaces
+{
+    public interface IAccountRepository
+    {
+        Task<Account?> GetByIdAsync(Guid id);
+        Task<Account?> GetByUserIdAsync(Guid userId);
+        Task AddAsync(Account account);
+        void Update(Account account);
+    }
+}
---
+++ src/AuraPay.Domain/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,15 @@
+﻿using AuraPay.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraPay.Domain.Interfaces
+{
+    public interface ITransactionRepository
+    {
+        Task AddAsync(Transaction transaction);
+        Task<IEnumerable<Transaction>> GetByAccountIdAsync(Guid accountId);
+    }
+}
