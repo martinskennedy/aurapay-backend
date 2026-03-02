ebde32261f276c4e33708e8e9152862ebfa1bc8e
Kennedy Martins <kemartins@gmail.com>
2026-03-02 15:28:45 -0300

    feat: add entitie User link Account entity to User entity and add IUserRepository

---
+++ src/AuraPay.Domain/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraPay.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
+        public string Document { get; private set; } // CPF no Brasil
+        public Guid ExternalId { get; private set; } // O ID que vem do Supabase (Auth)
+
+        public Account? Account { get; private set; }
+
+        protected User() { }
+
+        public User(string fullName, string email, string document, Guid externalId)
+        {
+            Id = Guid.NewGuid();
+            FullName = fullName;
+            Email = email;
+            Document = document;
+            ExternalId = externalId;
+        }
+    }
+}
---
+++ src/AuraPay.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using AuraPay.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraPay.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id);
+        Task<User?> GetByExternalIdAsync(Guid externalId); // Para buscar pelo ID do Supabase
+        Task<User?> GetByEmailAsync(string email);
+        Task AddAsync(User user);
+    }
+}
