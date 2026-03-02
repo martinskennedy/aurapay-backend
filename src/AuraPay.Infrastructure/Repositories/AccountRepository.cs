3c24ee5666a51169eee869371ab69ed3ca0f954b
Kennedy Martins <kemartins@gmail.com>
2026-03-02 10:48:05 -0300

    feat: implement account and transaction repositories

---
+++ src/AuraPay.Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,42 @@
+﻿using AuraPay.Domain.Entities;
+using AuraPay.Domain.Interfaces;
+using AuraPay.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraPay.Infrastructure.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly AuraPayDbContext _context;
+
+        public AccountRepository(AuraPayDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Account?> GetByIdAsync(Guid id)
+        {
+            return await _context.Accounts.FindAsync(id);
+        }
+
+        public async Task<Account?> GetByUserIdAsync(Guid userId)
+        {
+            return await _context.Accounts.FirstOrDefaultAsync(a => a.UserId == userId); // UserId vem do Supabase Auth
+        }
+
+        public async Task AddAsync(Account account)
+        {
+            await _context.Accounts.AddAsync(account);
+        }
+
+        public void Update(Account account)
+        {
+            _context.Accounts.Update(account);
+        }
+    }
+}
---
+++ src/AuraPay.Infrastructure/Repositories/TransactionRepository.cs
@@ -0,0 +1,35 @@
+﻿using AuraPay.Domain.Entities;
+using AuraPay.Domain.Interfaces;
+using AuraPay.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuraPay.Infrastructure.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly AuraPayDbContext _context;
+
+        public TransactionRepository(AuraPayDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Transaction transaction)
+        {
+            await _context.Transactions.AddAsync(transaction);
+        }
+
+        public async Task<IEnumerable<Transaction>> GetByAccountIdAsync(Guid accountId)
+        {
+            return await _context.Transactions
+                .Where(t => t.AccountId == accountId)
+                .OrderByDescending(t => t.Timestamp)
+                .ToListAsync();
+        }
+    }
+}
